471d1179fe8633e0284cc0a8cd860677923f3575
unknown <altamash rashid>
2024-11-07 16:43:59 +0530

    side menu toggle fixed

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -9,7 +9,7 @@
             <span class="navbar-toggler-icon"></span>
         </button> --}}
         <div class="flex justify_between items_center w_full">
-            <div class="toggle_icon_wrapper">
+            <div class="toggle_icon_wrapper hide-menu">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-text-left toggle_icon" viewBox="0 0 16 16" alt="Toggle Menu">
                     <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
                 </svg>

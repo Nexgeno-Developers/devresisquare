2247d20208042936094a3ae83caf2a14c8c02772
Umair <umair.makent@gmail.com>
2024-11-19 20:42:40 +0530

    property detail view layout

---
+++ app/View/Components/backend/Tabs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\backend;
+use Illuminate\View\Component;
+
+class Tabs extends Component
+{
+    public $tabs;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param array $tabs
+     * @return void
+     */
+    public function __construct($tabs)
+    {
+        $this->tabs = $tabs;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('backend.components.tabs');
+    }
+}
--- resources/views/backend/properties/index.blade.php
+++ resources/views/backend/properties/index.blade.php
@@ -2,6 +2,47 @@
 
 @section('content')
 
+<style>
+    .pv_tabs {
+    font-family: Arial, sans-serif;
+}
+
+.pv_tabs ul {
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+.pv_tabs ul li {
+    margin-right: 20px;
+}
+
+.pv_tabs ul li a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    padding: 10px;
+}
+
+.pv_tabs ul li a.active {
+    color: #007bff;
+    border-bottom: 2px solid #007bff;
+}
+
+.tab-content {
+    margin-top: 20px;
+}
+
+.tab-pane {
+    display: none;
+}
+
+.tab-pane.active {
+    display: block;
+}
+
+</style>
 
 <div class="row">
     <div class="col-lg-5">
@@ -33,7 +74,7 @@
                     type="{{$property['property_type']}}"
                     available="{{$property['available_from']}}"
                     price="{{$property['price']}}"
-                />
+                    propertyId="{{ $property['id'] }}"                    />
                 @endforeach
 
             </div>
@@ -43,153 +84,34 @@
     </div>
     <div class="col-lg-7">
         <div class="pv_detail_wrapper">
-            <div class="pv_tabs">
-                <ul>
-                    <li> <a href="" class="active"> Property </a></li>
-                    <li> <a href="" class=""> Owners </a></li>
-                    <li> <a href="" class=""> Offers </a></li>
-                    <li> <a href="" class=""> Complience </a></li>
-                    <li> <a href="" class=""> Tenancy </a></li>
-                    <li> <a href="" class=""> APS </a></li>
-                    <li> <a href="" class=""> Media </a></li>
-                    <li> <a href="" class=""> Teams </a></li>
-                    <li> <a href="" class=""> Contractor </a></li>
-                    <li> <a href="" class=""> Work Offer </a></li>
-                    <li> <a href="" class=""> Note </a></li>
-                </ul>
-            </div>
+
+            <x-backend.tabs :tabs="[
+                ['name' => 'Property', 'content' => 'Property details here...'],
+                ['name' => 'Owners', 'content' => 'Owner details here...'],
+                ['name' => 'Offers', 'content' => 'Offers details here...'],
+                ['name' => 'Complience', 'content' => 'Complience details here...'],
+                ['name' => 'Tenancy', 'content' => 'Tenancy details here...'],
+                ['name' => 'APS', 'content' => 'APS details here...'],
+                ['name' => 'Media', 'content' => 'Media details here...'],
+                ['name' => 'Teams', 'content' => 'Team details here...'],
+                ['name' => 'Contractor', 'content' => 'Contractor details here...'],
+                ['name' => 'Work Offer', 'content' => 'Work Offer details here...'],
+                ['name' => 'Note', 'content' => 'Note details here...']
+            ]" />
             <div class="pv_detail_content">
                 <div class="pv_detail_header">
-                    <div class="pv_main_title">Property Detail</div>
+                    <div class="pv_main_title">{{--$tabname--}} Detail</div>
                     <div class="pvdh_btns_wrapper">
-                        <x-backend.link-button
-                            class=""
-                            name="Add Tenancy"
-                            link="{{ route('admin.properties.quick') }}"
-                            onClick=""
-                        />
-                        <x-backend.link-button
-                            class=""
-                            name="Add Offer"
-                            link="{{ route('admin.properties.quick') }}"
-                            onClick=""
-                        />
-                        <x-backend.outline-link-button
-                            class=""
-                            name="Edit Property"
-                            link="{{ route('admin.properties.edit', ['id' => $property->id]) }}"
-                            onClick=""
-                        />
+                        <x-backend.link-button class="" name="Add Tenancy" link="{{ route('admin.properties.quick') }}"
+                            onClick="" />
+                        <x-backend.link-button class="" name="Add Offer" link="{{ route('admin.properties.quick') }}"
+                            onClick="" />
+                        <x-backend.outline-link-button class="" name="Edit Property"
+                            link="{{ route('admin.properties.edit', ['id' => $property->id]) }}" onClick="" />
                     </div>
                 </div>
                 <div class="pv_content_detail">
-
-                    <div class="pvd_content_wrapper">
-                        <div class="pv_image">
-                            <img src="{{ asset('/asset/images/temp-property.webp') }}" alt="property">
-                        </div>
-                        <div class="pv_content">
-                            <div class="pvc_ref_id">Ref: 1234SSSD</div>
-                            <div class="pvc_poperty_name">169-173 Portland Rd, Hove, East Sussex, BN3 5QJ</div>
-                            <div class="rs_property_icons">
-                                <div class="bed_icon rs_tooltip">
-                                    <img src=" {{asset('asset/images/svg/icons/bed.svg')}} " alt="bedroom" > 2
-                                </div>
-                                <div class="bath_icon rs_tooltip">
-                                    <img src=" {{asset('asset/images/svg/icons/bath.svg')}} " alt="bathroom"> 1
-                                </div>
-                                <div class="floors_icon rs_tooltip">
-                                    <img src=" {{asset('asset/images/svg/icons/floor.svg')}} " alt="Floors">2
-                                </div>
-                                <div class="living_icon rs_tooltip">
-                                    <img src=" {{asset('asset/images/svg/icons/sofa.svg')}} " alt="sofa"> 2
-                                </div>
-                            </div>
-                            <div class="pvc_price">
-                                Price: <span>£3000</span>
-                            </div>
-                            <div class="rs_row">
-                                <div class="rs_col">
-                                    <div class="pv_type">Type: <strong> Apparment</strong></div>
-                                </div>
-                                <div class="rs_col">
-                                    <div class="pv_availability">Availability: <strong>11/02/25</strong></div>
-                                </div>
-                            </div>
-                            {{-- rs_row end  --}}
-                            <div class="rs_row">
-                                <div class="rs_col">
-                                    <div class="pv_status">Status: <strong> For Sale</strong></div>
-                                </div>
-                                <div class="rs_col">
-                                    <div class="pv_service">Service: <strong>Let Only</strong></div>
-                                </div>
-                            </div>
-                            {{-- rs_row end  --}}
-
-                        </div>
-                        {{-- pv_content end  --}}
-                    </div>
-                    {{-- pvd_content_wrapper end --}}
-                    <div class="pvd_other_content border_bottom">
-                        <div class="row">
-                            <div class="col-lg-4 col-6">
-                                <div class="row">
-                                    <div class="col-lg-5 col-12 mb-2">Furnished</div>
-                                    <div class="col-lg-7 col-12 mb-2">Unfurnished</div>
-                                    <div class="col-lg-5 col-12 mb-2">Parking</div>
-                                    <div class="col-lg-7 col-12 mb-2">Yes</div>
-                                    <div class="col-lg-5 col-12 mb-2">Balcony</div>
-                                    <div class="col-lg-7 col-12 mb-2">Yes</div>
-                                    <div class="col-lg-5 col-12 mb-2">Garden</div>
-                                    <div class="col-lg-7 col-12 mb-2">Yes</div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-6">
-                                <div class="row">
-                                    <div class="col-lg-6 col-12 mb-2">Collecting Rent</div>
-                                    <div class="col-lg-6 col-12 mb-2">Yes</div>
-                                    <div class="col-lg-6 col-12 mb-2">Area Sqr. Feet</div>
-                                    <div class="col-lg-6 col-12 mb-2">800</div>
-                                    <div class="col-lg-6 col-12 mb-2">Area Sqr. Meter</div>
-                                    <div class="col-lg-6 col-12 mb-2">20</div>
-                                    <div class="col-lg-6 col-12 mb-2">Aspects</div>
-                                    <div class="col-lg-6 col-12 mb-2">North</div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    {{-- pvd_other_content end --}}
-                    <div class="pvd_features">
-                        <div class="pv_sub_title mb-4">
-                            Features
-                        </div>
-                        <div class="row features_list_warpper">
-                            <div class="col-lg-6">
-                                <ul class="features_list">
-                                    <li>Frunished</li>
-                                    <li>Flexible</li>
-                                    <li>Undercounter refrigerator with freezer</li>
-                                    <li>Dishwasher</li>
-                                    <li>Gas hob</li>
-                                    <li>Washing machine</li>
-                                    <li>Dryer</li>
-                                </ul>
-                            </div>
-                            <div class="col-lg-6">
-                                <ul class="features_list">
-                                    <li>Window locks</li>
-                                    <li>Business Centre</li>
-                                    <li>Double glazing</li>
-                                    <li>Underfloor heating</li>
-                                    <li>Dishwasher</li>
-                                    <li>Gas</li>
-                                    <li>Central heating</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    {{-- render first tabs blade file from view example @include('backend.properties.tabs' . $tabname) $tabname in small case--}}
                 </div>
             </div>
         </div>
@@ -199,6 +121,19 @@
 
 @section('page.scripts')
 <script>
+    document.querySelectorAll('.tab-link').forEach(tab => {
+        tab.addEventListener('click', function(event) {
+            event.preventDefault();
 
+            // Remove active class from all tabs and tab content
+            document.querySelectorAll('.tab-link').forEach(link => link.classList.remove('active'));
+            document.querySelectorAll('.tab-pane').forEach(pane => pane.classList.remove('active'));
+
+            // Add active class to the clicked tab and corresponding content
+            this.classList.add('active');
+            document.getElementById(this.getAttribute('href').substring(1)).classList.add('active');
+        });
+    });
 </script>
 @endsection
+

cd570898057b8913360623b55bcae3e60c05b777
Altamash Rashid <asr.designmaster@gmail.com>
2024-11-24 16:20:50 +0530

    css incluedded in one file

--- resources/views/backend/layout/app.blade.php
+++ resources/views/backend/layout/app.blade.php
@@ -12,12 +12,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('asset/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('asset/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('asset/css/toastr.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('asset/css/toastr.min.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('asset/backend/css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- <link href="{{ asset('asset/backend/css/aiz-core.css') }}" rel="stylesheet"> --}}
-    <link href="{{ asset('asset/backend/css/media.css') }}" rel="stylesheet">
-    <link href="{{ asset('asset/backend/css/vendors.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('asset/backend/css/media.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('asset/backend/css/vendors.css') }}" rel="stylesheet"> --}}
 </head>
 
 <body class="show-sidebar">

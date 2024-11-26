7f92ea9e99658a49f47303c5c132a33d6f9381de
Altamash Rashid <asr.designmaster@gmail.com>
2024-11-26 17:32:49 +0530

    owners tab added

--- resources/views/backend/properties/tabs/owners.blade.php
+++ resources/views/backend/properties/tabs/owners.blade.php
@@ -1,5 +1,8 @@
 @php
-    echo '<pre>';
-    var_dump($propertyId);
-    echo '</pre>';
+    $headers = ['id','Name', 'Position', 'Phone', 'email', 'City'];
+    $rows = [
+        [1, 'John Doe', 'Owner', '456798462', 'john@example.com', 'London'],
+        [2, 'Jane Smith', 'Owner', '974511268', 'jane@example.com', 'Mumbai'],
+    ];
 @endphp
+<x-backend.dynamic-table :headers="$headers" :rows="$rows" class='' />

4adfa5938b7fa9aaf8f43c9c295e75741f433495
Altamash Rashid <asr.designmaster@gmail.com>
2024-11-28 13:43:00 +0530

    table mobile show  component updated

--- resources/views/backend/components/common/table-new.blade.php
+++ resources/views/backend/components/common/table-new.blade.php
@@ -31,4 +31,31 @@
             </tbody>
         </table>
     </div>
+    {{-- desktop_only end  --}}
+    <div class="mobile_only">
+        <div class="rs_mobile_table">
+            <div class="flex justify_between align_center">
+                <!-- Headers -->
+                <div class="rsmt_left_col">
+                    @foreach ($headers as $header)
+                        @if ($header !== 'id')
+                            <div>{{ $header }}</div>
+                        @endif
+                    @endforeach
+                </div>
+                <!-- Values -->
+                <div class="rsmt_right_col">
+                    @foreach ($row as $key => $value)
+                        @if ($key !== 'id')
+                            <div>{{ $value}}</div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+    {{-- mobile_only end  --}}
 </div>

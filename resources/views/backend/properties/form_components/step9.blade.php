cbba3bf015c611bddd293d6503f75a7acdc3529d
Altamash Rashid <asr.designmaster@gmail.com>
2024-12-02 14:54:28 +0530

    responsibility fixes

--- resources/views/backend/properties/form_components/step9.blade.php
+++ resources/views/backend/properties/form_components/step9.blade.php
@@ -8,62 +8,66 @@
 
     <label class="main_title">Responsibility</label>
 
-    <div class="steps_wrapper property-form-data-attribute" data-step-name="Responsibility" data-step-number="{{ $currentStep }}" data-step-title="Responsibility">
+    <div class="row">
+        <div class="col-lg-4 col-12">
+            <div class="steps_wrapper property-form-data-attribute" data-step-name="Responsibility" data-step-number="{{ $currentStep }}" data-step-title="Responsibility">
 
 
-        <div class="form-group">
-            <label for="designation">Designation</label>
-            <select name="designation" id="designation" class="form-control" required>
-                <option value="" disabled {{ (isset($property) && $property->designation == '') ? 'selected' : '' }}>Select a service</option>
-                <option value="estate_agent" {{ (isset($property) && $property->designation == 'estate_agent') ? 'selected' : '' }}>Estate Agent</option>
-                <option value="landlord" {{ (isset($property) && $property->designation == 'landlord') ? 'selected' : '' }}>Landlord</option>
-                <option value="tenant" {{ (isset($property) && $property->designation == 'tenant') ? 'selected' : '' }}>Tenant</option>
-                <option value="manager" {{ (isset($property) && $property->designation == 'manager') ? 'selected' : '' }}>Manager</option>
-            </select>
-            @error('designation')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                <div class="form-group">
+                    <label for="designation">Designation</label>
+                    <select name="designation" id="designation" class="form-control" required>
+                        <option value="" disabled {{ (isset($property) && $property->designation == '') ? 'selected' : '' }}>Select a service</option>
+                        <option value="estate_agent" {{ (isset($property) && $property->designation == 'estate_agent') ? 'selected' : '' }}>Estate Agent</option>
+                        <option value="landlord" {{ (isset($property) && $property->designation == 'landlord') ? 'selected' : '' }}>Landlord</option>
+                        <option value="tenant" {{ (isset($property) && $property->designation == 'tenant') ? 'selected' : '' }}>Tenant</option>
+                        <option value="manager" {{ (isset($property) && $property->designation == 'manager') ? 'selected' : '' }}>Manager</option>
+                    </select>
+                    @error('designation')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-        <div class="form-group">
-            <label for="branch">Branch</label>
-            <select name="branch" id="branch" class="form-control" required>
-                <option value="" disabled {{ (isset($property) && $property->branch == '') ? 'selected' : '' }}>Select a service</option>
-                <option value="branch1" {{ (isset($property) && $property->branch == 'branch1') ? 'selected' : '' }}>Branch1</option>
-                <option value="branch2" {{ (isset($property) && $property->branch == 'branch2') ? 'selected' : '' }}>Branch2</option>
-                <option value="branch3" {{ (isset($property) && $property->branch == 'branch3') ? 'selected' : '' }}>Branch3</option>
-                <option value="branch4" {{ (isset($property) && $property->branch == 'branch4') ? 'selected' : '' }}>Branch4</option>
-            </select>
-            @error('branch')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                <div class="form-group">
+                    <label for="branch">Branch</label>
+                    <select name="branch" id="branch" class="form-control" required>
+                        <option value="" disabled {{ (isset($property) && $property->branch == '') ? 'selected' : '' }}>Select a service</option>
+                        <option value="branch1" {{ (isset($property) && $property->branch == 'branch1') ? 'selected' : '' }}>Branch1</option>
+                        <option value="branch2" {{ (isset($property) && $property->branch == 'branch2') ? 'selected' : '' }}>Branch2</option>
+                        <option value="branch3" {{ (isset($property) && $property->branch == 'branch3') ? 'selected' : '' }}>Branch3</option>
+                        <option value="branch4" {{ (isset($property) && $property->branch == 'branch4') ? 'selected' : '' }}>Branch4</option>
+                    </select>
+                    @error('branch')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-        <div class="form-group">
-            <label for="commission_percentage">Commission (%)</label>
-            <input required type="text" name="commission_percentage" id="commission_percentage" class="form-control" value="{{ (isset($property) && $property->commission_percentage) ? $property->commission_percentage : '' }}">
-            @error('commission_percentage')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                <div class="form-group">
+                    <label for="commission_percentage">Commission (%)</label>
+                    <input required type="text" name="commission_percentage" id="commission_percentage" class="form-control" value="{{ (isset($property) && $property->commission_percentage) ? $property->commission_percentage : '' }}">
+                    @error('commission_percentage')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-        <div class="form-group">
-            <label for="commission_amount">Commission (£)</label>
-            <div class="price_input_wrapper">
-                <div class="pound_sign">£</div>
-                <input required type="text" name="commission_amount" id="commission_amount" class="form-control" value="{{ (isset($property) && $property->commission_amount) ? $property->commission_amount : '' }}">
+                <div class="form-group">
+                    <label for="commission_amount">Commission (£)</label>
+                    <div class="price_input_wrapper">
+                        <div class="pound_sign">£</div>
+                        <input required type="text" name="commission_amount" id="commission_amount" class="form-control" value="{{ (isset($property) && $property->commission_amount) ? $property->commission_amount : '' }}">
+                    </div>
+                    @error('commission_amount')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}" data-current-step="{{ $currentStep }}">Previous</button>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <button type="submit" class="btn btn-primary w-100 last-step-submit" data-current-step="{{ $currentStep }}">Submit</button>
+                </div>
             </div>
-            @error('commission_amount')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}" data-current-step="{{ $currentStep }}">Previous</button>
-            </div>
-            <div class="col-12 col-md-6">
-                <button type="submit" class="btn btn-primary w-100 last-step-submit" data-current-step="{{ $currentStep }}">Submit</button>
         </div>
     </div>
 

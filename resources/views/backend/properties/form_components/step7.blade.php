c6fa65e8dc6e07de8d20b57c975a615e91db9b4e
Altamash Rashid <asr.designmaster@gmail.com>
2024-12-02 14:52:26 +0530

    step media fixes

--- resources/views/backend/properties/form_components/step7.blade.php
+++ resources/views/backend/properties/form_components/step7.blade.php
@@ -18,42 +18,47 @@ if(isset($property)){
 
     <div class="property-form-data-attribute" data-step-name="Valid EPC" data-step-number="{{ $currentStep }}" data-step-title="Valid EPC"></div>
     
-    <div class="steps_wrapper">
-        <div class="form-group">
-            <label for="epc_rating">EPC Rating</label>
-            <select name="epc_rating" id="epc_rating" class="form-control">
-                <option value="A" {{ $epcRating == 'A' ? 'selected' : '' }}>A</option>
-                <option value="B" {{ $epcRating == 'B' ? 'selected' : '' }}>B</option>
-                <option value="C" {{ $epcRating == 'C' ? 'selected' : '' }}>C</option>
-                <option value="D" {{ $epcRating == 'D' ? 'selected' : '' }}>D</option>
-            </select>
-            @error('epc_rating')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
+    <div class="row">
+        <div class="col-lg-6 col-12">
 
-        <div class="form-group">
-            <label>Does the property have gas?</label>
-            <div class="rs_radio_btns">
-                <div>
-                    <input type="radio" name="is_gas" id="is_gas_no" value="0" {{ $isGas == '0' ? 'checked' : '' }} /> 
-                    <label for="is_gas_no" > No</label>
+            <div class="steps_wrapper">
+                <div class="form-group">
+                    <label for="epc_rating">EPC Rating</label>
+                    <select name="epc_rating" id="epc_rating" class="form-control">
+                        <option value="A" {{ $epcRating == 'A' ? 'selected' : '' }}>A</option>
+                        <option value="B" {{ $epcRating == 'B' ? 'selected' : '' }}>B</option>
+                        <option value="C" {{ $epcRating == 'C' ? 'selected' : '' }}>C</option>
+                        <option value="D" {{ $epcRating == 'D' ? 'selected' : '' }}>D</option>
+                    </select>
+                    @error('epc_rating')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
-                <div>
-                    <input type="radio" name="is_gas" id="is_gas_yes" value="1" {{ $isGas == '1' ? 'checked' : '' }} /> 
-                    <label for="is_gas_yes" > Yes</label>
+
+                <div class="form-group">
+                    <label>Does the property have gas?</label>
+                    <div class="rs_radio_btns">
+                        <div>
+                            <input type="radio" name="is_gas" id="is_gas_no" value="0" {{ $isGas == '0' ? 'checked' : '' }} /> 
+                            <label for="is_gas_no" > No</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="is_gas" id="is_gas_yes" value="1" {{ $isGas == '1' ? 'checked' : '' }} /> 
+                            <label for="is_gas_yes" > Yes</label>
+                        </div>
+                        @error('is_gas')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
                 </div>
-                @error('is_gas')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}" data-current-step="{{ $currentStep }}">Previous</button>
-            </div>
-            <div class="col-12 col-md-6">
-                <button type="button" class="btn btn-primary w-100 next-step" data-next-step="{{ $currentStep + 1 }}" data-current-step="{{ $currentStep }}">Next</button>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}" data-current-step="{{ $currentStep }}">Previous</button>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <button type="button" class="btn btn-primary w-100 next-step" data-next-step="{{ $currentStep + 1 }}" data-current-step="{{ $currentStep }}">Next</button>
+                </div> 
+            </div> 
         </div> 
     </div> 
 </form>
--- resources/views/backend/properties/form_components/step8.blade.php
+++ resources/views/backend/properties/form_components/step8.blade.php
@@ -7,7 +7,7 @@
     <input type="hidden" id="property_id" class="property_id" name="property_id"
         value="{{ session('property_id') ?? (isset($property) ? $property->id : '') }}">
 
-    <label class="main_title">Media</label>
+    <div class="main_title">Media</div>
 
     <div class="steps_wrapper" data-step-name="Media" data-step-number="{{ $currentStep }}" data-step-title="Media">
 {{--
@@ -20,7 +20,7 @@
                         <!-- Preview images will be dynamically added here -->
                     </div>
                     <div class="media_upload">
-                        <label for="photos">Upload Photos</label>
+                        <div for="photos">Upload Photos</div>
                         <input type="file" name="photos[]" id="photos" class="form-control" multiple accept="image/*"
                             onchange="previewMultipleImage(this)">
                         @error('photos.*')
@@ -58,12 +58,12 @@
             <h5 class="sub_title mt-4">Photos</h5>
             <div class="media_wrapper">
                 <div class="input-group" data-toggle="aizuploader" data-type="image" data-multiple="true">
-                    <label class="col-form-label" for="signinSrEmail">Photos</label>
+                    <label class="col-form-label" for="photos">Photos</label>
                     <div class="d-none input-group-prepend">
                         <div class="input-group-text bg-soft-secondary font-weight-medium">Browse</div>
                     </div>
                     <div class="d-none form-control file-amount">Choose File</div>
-                    <input id="sort-the-photo" type="hidden" name="photos" value="{{ isset($property) && isset($property->photos) ? $property->photos : '' }}" class="selected-files">
+                    <input id="photos" id="photos" type="hidden" name="photos" value="{{ isset($property) && isset($property->photos) ? $property->photos : '' }}" class="selected-files">
                 </div>
                 <div class="d-flex gap-3 file-preview box sm">
                 </div>
@@ -79,7 +79,7 @@
                         <div class="input-group-text bg-soft-secondary font-weight-medium">Browse</div>
                     </div>
                     <div class="d-none form-control file-amount">Choose File</div>
-                    <input id="sort-the-photo" type="hidden" name="floor_plan" value="{{ isset($property) && isset($property->floor_plan) ? $property->floor_plan : '' }}" class="selected-files">
+                    <input id="floor_plan" type="hidden" name="floor_plan" value="{{ isset($property) && isset($property->floor_plan) ? $property->floor_plan : '' }}" class="selected-files">
                 </div>
                 <div class="d-flex gap-3 file-preview box sm">
                 </div>
@@ -95,30 +95,34 @@
                         <div class="input-group-text bg-soft-secondary font-weight-medium">Browse</div>
                     </div>
                     <div class="d-none form-control file-amount">Choose File</div>
-                    <input id="sort-the-photo" type="hidden" name="view_360" value="{{ isset($property) && isset($property->view_360) ? $property->view_360 : '' }}" class="selected-files">
+                    <input type="hidden" id="view_360" name="view_360" value="{{ isset($property) && isset($property->view_360) ? $property->view_360 : '' }}" class="selected-files">
                 </div>
                 <div class="d-flex gap-3 file-preview box sm">
                 </div>
             </div>
         </div>
 
-        <div class="form-group rs_upload_btn">
-            <h5 class="sub_title mt-4">Video URL</h5>
-            <label for="video_url">Video URL</label>
-            <input required type="url" name="video_url" id="video_url" class="form-control" value="{{ isset($property) && $property->video_url ? $property->video_url : '' }}">
-            @error('video_url')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
         <div class="row">
-            <div class="col-12 col-md-6">
-                <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}"
-                    data-current-step="{{ $currentStep }}">Previous</button>
-            </div>
-            <div class="col-12 col-md-6">
-                <button type="button" class="btn btn-primary w-100 next-step" data-next-step="{{ $currentStep + 1 }}"
-                    data-current-step="{{ $currentStep }}">Next</button>
+            <div class="col-lg-6 col-12">
+                <div class="form-group rs_upload_btn">
+                    <h5 class="sub_title mt-4">Video URL</h5>
+                    <label for="video_url">Video URL</label>
+                    <input required type="url" name="video_url" id="video_url" class="form-control" value="{{ isset($property) && $property->video_url ? $property->video_url : '' }}">
+                    @error('video_url')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+        
+                <div class="row mt-4">
+                    <div class="col-12 col-md-6">
+                        <button type="button" class="btn btn-secondary w-100 previous-step" data-previous-step="{{ $currentStep - 1 }}"
+                            data-current-step="{{ $currentStep }}">Previous</button>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <button type="button" class="btn btn-primary w-100 next-step" data-next-step="{{ $currentStep + 1 }}"
+                            data-current-step="{{ $currentStep }}">Next</button>
+                    </div>
+                </div>
             </div>
         </div>
 </form>

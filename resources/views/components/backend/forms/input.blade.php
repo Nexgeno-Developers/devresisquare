c7d03ac10651def83a348a0196cd2600f436972d
Altamash Rashid <asr.designmaster@gmail.com>
2024-12-07 01:32:58 +0530

    input component

---
+++ resources/views/components/backend/forms/input.blade.php
@@ -0,0 +1,33 @@
+
+@props([
+   'class', 
+   'inputName'=>null, 
+   'inputOpt'=>null,
+   'inputType'=>'text',
+   'rightIcon'=>null,
+   'placeHolder'=>null,
+   'isLabel',
+   'label'=>null,
+   'isDate'=>false,
+   'isIcon'=>false,
+   'iconName', 'onIconClick'=>''
+])
+
+
+@if($isLabel == True)
+<label class="mb-2" for="{{ $inputName }}">{{ $label }}</label>
+@endif
+<div class="{{$attributes->merge([ 'class' => 'rs_input'])}}  {{ $inputOpt }} {{ $isLabel == True ? "with_label" : ""}} {{ $isDate == True? "with_date" : "" }} ">
+   @if ($inputOpt == 'input_price')
+      {{ getPoundSymbol() }}
+   @endif
+
+   <input type="{{ $inputType }}" name= "{{ $inputName }}" placeholder="{{ $placeHolder}}" />
+
+   @if ($rightIcon)
+   <div class="right_icon">{{ $rightIcon }}</div>
+   @endif
+   @if ($isIcon)
+   <div class="right_icon" onClick="{{ $onIconClick }}()"><i class="bi {{ $iconName }}"></i></div>
+   @endif
+</div>

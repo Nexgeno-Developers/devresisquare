f1c077dc398cc0b86c524e89a0355a9858288fce
Altamash Rashid <asr.designmaster@gmail.com>
2024-12-19 19:57:28 +0530

    design update

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -10,19 +10,17 @@
   
                 <form action="{{ route('login.post') }}" method="POST">
                     @csrf
-                    <div class="mb-md-5 mt-md-4 pb-5">
+                    <div class="mb-md-5 mt-md-4 pb-4">
         
                         <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
                         <p class=" mb-5">Please enter your login and password!</p>
         
                         <div class="form-outline form-white mb-4">
-                            <input type="email" class="form-control form-control-lg" id="email" name="email" required />
-                            <label class="form-label" for="email">Email</label>
+                            <input type="email" class="form-control form-control-lg" id="email" name="email" placeholder="Email" required />
                         </div>
         
                         <div class="form-outline form-white mb-4">
-                            <input type="password" class="form-control form-control-lg" id="password" name="password" required />
-                            <label class="form-label" for="password">Password</label>
+                            <input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Password" required />
                         </div>
         
                         <p class="small mb-5 pb-lg-2"><a class="" href="#!">Forgot password?</a></p>
@@ -32,7 +30,7 @@
                     </div>
         
                     <div>
-                        <p class="mb-0">Don't have an account? <a href="#!" class=" fw-bold">Sign Up</a>
+                        <p class="mb-0">Don't have an account? <a href="{{ route('register.post') }}" class=" fw-bold">Sign Up</a>
                         </p>
                     </div>
                 </form>
